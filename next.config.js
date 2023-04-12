acdb1416d3e07788b95337e83be92bc89af3fb4c
Hemant Juyal <hemantjuyal@gmail.com>
2023-04-12 11:20:08 +0530

    updated files

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['nftmarkitplace.infura-ipfs.io'],
+    domains: ['<NEXT_PUBLIC_DEDICATED_GATEWAY from .env.local>'],
   },
 }
 
